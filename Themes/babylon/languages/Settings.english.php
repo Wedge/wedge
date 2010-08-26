ce960d8c9b72107778a040b6f97b957fee71fbad
Nao <nao@wedge>
2010-08-26 17:06:06 +0000

    - Beat it, Classic! Skedaddle, Core villains! Be gone, whores of Babylon! There is only room for one theme and her curves!

--- Themes/babylon/languages/Settings.english.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Version: 2.0 RC3; Settings
-
-// Important! Before editing these language files please read the text at the topic of index.english.php.
-
-$txt['theme_thumbnail_href'] = $settings['images_url'] . '/thumbnail.gif';
-$txt['theme_description'] = 'The babylon theme from Simple Machines.<br /><br />Thanks go to Babylonking and Alienine.';
-
-?>
--- Themes/classic/languages/Settings.english.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Version: 2.0 RC3; Settings
-
-// Important! Before editing these language files please read the text at the topic of index.english.php.
-
-$txt['theme_thumbnail_href'] = $settings['images_url'] . '/thumbnail.gif';
-$txt['theme_description'] = 'The classic look that introduced the groundbreaking PHP/MySQL port of YaBB and distinguished it through two years of development until its rebirth as SMF.<br /><br />Author: <em><a href="mailto:webmaster@yabbse.org">The YaBB SE Team</a></em>.';
-
-?>
