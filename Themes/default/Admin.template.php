bcdbc92c032f68241430f30673f9fdeeecdb25a0
Nao <nao@wedge>
2012-01-11 10:44:27 +0000

    ! Undefined index error (thanks John). (Admin.template.php) ! The footer logo was incorrect in Wuthering. (Wuthering/index.css)

--- Themes/default/Admin.template.php
+++ Themes/default/Admin.template.php
@@ -737,7 +737,7 @@ function template_show_settings()
 						<select name="', $config_var['name'], '" id="', $config_var['name'], '" ', $javascript, $disabled, !empty($config_var['multiple']) ? ' multiple' : '', '>';
 					foreach ($config_var['data'] as $option)
 						echo '
-							<option value="', $option[0], '"', $option[0] == $config_var['value'] || (!empty($config_var['multiple']) && in_array($option[0], $config_var['value'])) ? ' selected' : '', '>', $option[1], '</option>';
+							<option value="', $option[0], '"', $option[0] == $config_var['value'] || (!empty($config_var['multiple']) && isset($config_var['value']) && in_array($option[0], $config_var['value'])) ? ' selected' : '', '>', $option[1], '</option>';
 					echo '
 						</select>';
 				}
