cdc4df5ba3b06086ad2354043e1408999d45af92
Arantor <sleeping@myperch.org>
2012-03-14 23:41:25 +0000

    ! Sorry Nao, I don't think you'll like this regex change much. But we need to make sure that canonical URLs do not get passed through the PHPSESSID injector, since prior to this, they were - which means PHPSESSIDs galore throughout the web, even with canonical URLs. Yes, SMF has this bug but too many people keep denying its existence without actually bothering to check. (Subs-Template.php)

--- Sources/Subs-Template.php
+++ Sources/Subs-Template.php
@@ -158,7 +158,7 @@ function ob_sessrewrite($buffer)
 
 	// Do nothing if the session is cookied, or they are a crawler - guests are caught by redirectexit().
 	if (empty($_COOKIE) && SID != '' && empty($context['browser']['possibly_robot']))
-		$buffer = preg_replace('~"' . $preg_scripturl . '(?!\?' . preg_quote(SID, '~') . ')\\??~', '"' . $scripturl . '?' . SID . '&amp;', $buffer);
+		$buffer = preg_replace('~(?<!<link rel="canonical" href=)"' . $preg_scripturl . '(?!\?' . preg_quote(SID, '~') . ')\\??~', '"' . $scripturl . '?' . SID . '&amp;', $buffer);
 	// Debugging templates, are we?
 	elseif (isset($_GET['debug']))
 		$buffer = preg_replace('~(?<!<link rel="canonical" href=)"' . $preg_scripturl . '\\??~', '"' . $scripturl . '?debug;', $buffer);
