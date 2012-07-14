6a18fe692db7a8373cb8fa4459d4d496114d602e
Nao <nao@wedge>
2012-07-14 20:50:37 +0000

    - Removed code tag's hack code from theme.js because I did some testing and couldn't find a single browser that had problems with these. I'll restore it if someone can provide me with a clean test case. (theme.js) * Moved img.resized JS code to the img tag parser, and got rid of theme.js contents in the process, saving about 200 gzipped bytes from the main script file. (install.sql, theme.js) * Replaced CRLF codes with simple Linux linefeeds in bbcode PHP. This should be just fine this way. (install.sql) * Ensuring that the install script declares all auto-incremented IDs as unsigned. I don't even know if it's possible to manually insert a negative ID in these... Probably possible, but pointless. (install.sql) * Started harmonizing field sizes in the database. For instance, id_member is mediumint(8) unsigned (I don't think there's any case where an id_member is stored as negative, even for a pseudo-member), things like that. (install.sql) + Added an id_media field to topics, for future use. (Yeah, don't have to be Einstein to get that one...) (install.sql)

--- Themes/default/scripts/theme.js
+++ Themes/default/scripts/theme.js
@@ -2,6 +2,7 @@
  * Wedge
  *
  * Functions required by the current theme.
+ * This demonstration file is empty -- feel free to remove it or provide one for your custom themes.
  *
  * @package wedge
  * @copyright 2010-2012 Wedgeward, wedge.org
@@ -9,26 +10,3 @@
  *
  * @version 0.1
  */
-
-// The purpose of this code is to fix the height of overflow: auto blocks,
-// because some browsers can't figure it out for themselves. (Opera FTW!)
-if (is_ie || is_webkit || is_ff)
-{
-	$('code').each(function () {
-		var cliwid = this.clientWidth, scrwid = this.scrollWidth, offhei = this.offsetHeight, sty = this.currentStyle, hei = sty ? sty.height : '';
-
-		if (is_webkit && offhei < 20)
-			this.style.height = (offhei + 20) + 'px';
-
-		else if (is_ff && (scrwid > cliwid || cliwid == 0))
-			this.style.overflow = 'scroll';
-
-		else if (sty && sty.overflow == 'auto' && (hei == '' || hei == 'auto') && (scrwid > cliwid || cliwid == 0) && (offhei != 0))
-			this.style.height = (offhei + 24) + 'px';
-	});
-}
-
-// Toggles the element height and width styles of an image.
-$('img.resized').css('cursor', 'pointer').click(function () {
-	this.style.width = this.style.height = (this.style.width == 'auto' ? null : 'auto');
-});
