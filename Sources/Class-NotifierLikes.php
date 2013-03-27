2f4e902afa911dd7c0b46068b3adb18a8dc87b9c
Arantor <sleeping@myperch.org>
2013-03-27 03:42:50 +0000

    ! Missing file from r2023. I clearly need more tea. (Class-NotifierLikes.php)

---
+++ Sources/Class-NotifierLikes.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Wedge
+ *
+ * Contains functions for notifying users based on likes.
+ *
+ * @package wedge
+ * @copyright 2010-2013 Wedgeward, wedge.org
+ * @license http://wedge.org/license/
+ *
+ * @version 0.1
+ */
+
+if (!defined('WEDGE'))
+	die('File cannot be requested directly.');
+
+class Likes_Notifier extends Notifier
+{
+	public function getURL(Notification $notification)
+	{
+		$data = $notification->getData();
+		return 'topic=' . $data['topic'] . '.msg' . $notification->getObject() . '#msg' . $notification->getObject();
+	}
+
+	public function getName()
+	{
+		return 'likes';
+	}
+
+	public function getText(Notification $notification)
+	{
+		global $txt;
+
+		$data = $notification->getData();
+
+		return sprintf($txt['welikes_notification'], $data['member']['name'], $data['subject']);
+	}
+
+    public function handleMultiple(Notification $notification, array &$data, array &$email_data)
+    {
+        return false;
+    }
+
+    public function getProfile($id_member)
+    {
+        global $txt;
+
+        return array($txt['welikes_title'], $txt['welikes_desc'], array());
+    }
+
+    public function getEmail(Notification $notification, array $email_data)
+    {
+        global $txt;
+
+        return array($txt['welikes_subject'], $this->getText($notification));
+    }
+}
