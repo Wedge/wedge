9fac65ffb17823731195b4ec015264894b5a12dd
Arantor <sleeping@myperch.org>
2013-06-05 00:01:05 +0000

    ! The notifier file I forgot to commit just now. (Move.php) ! The current contributors list. It's not 100% accurate at this point in time since we haven't updated getID3 to the MPL licenced version yet but we will be doing so shortly.

---
+++ Sources/notifiers/Move.php
@@ -0,0 +1,118 @@
+<?php
+
+if (!defined('WEDGE'))
+	die('File cannot be requested directly');
+
+class Move_Notifier extends Notifier
+{
+	/**
+	 * Callback for getting the URL of the object
+	 *
+	 * @access public
+	 * @param Notification $notification
+	 * @return string A fully qualified HTTP URL
+	 */
+	public function getURL(Notification $notification)
+	{
+		$data = $notification->getData();
+		return 'topic=' . $notification->getObject() . '.msg' . $data['id_msg'] . '#msg' . $data['id_msg'];
+	}
+
+	/**
+	 * Callback for getting the text to display on the notification screen
+	 *
+	 * @access public
+	 * @param Notification $notification
+	 * @return string The text this notification wants to display
+	 */
+	public function getText(Notification $notification)
+	{
+		global $txt;
+
+		$data = $notification->getData();
+
+		// Only one member?
+		$notif = in_array($data['id_board'], we::$user['qsb_boards']) ? 'notification_move' : 'notification_move_noaccess';
+		return strtr($txt[$notif], array(
+			'{MEMBER}' => $data['member']['name'],
+			'{SUBJECT}' => shorten_subject($data['subject'], 25),
+			'{BOARD}' => $data['board'],
+		));
+	}
+
+	/**
+	 * Returns the name of this notifier
+	 *
+	 * @access public
+	 * @return string
+	 */
+	public function getName()
+	{
+		return 'move';
+	}
+
+	/**
+	 * Callback for handling multiple notifications on the same object
+	 * We don't have any special criterion for this
+	 *
+	 * @access public
+	 * @param Notification $notification
+	 * @param array &$data Reference to the new notification's data, if something needs to be altered
+	 * @return bool, if false then a new notification is not created but the current one's time is updated
+	 */
+	public function handleMultiple(Notification $notification, array &$data, array &$email_data)
+	{
+		return false;
+	}
+
+	/**
+	 * Returns the elements for notification's profile area
+	 *
+	 * @access public
+	 * @param int $id_member The ID of the member whose profile is currently being accessed
+	 * @return array(title, description, config_vars)
+	 */
+	public function getProfile($id_member)
+	{
+		global $txt;
+
+		return array($txt['notification_move_profile'], $txt['notification_move_profile_desc'], array());
+	}
+
+	/**
+	 * E-mail handler
+	 *
+	 * @access public
+	 * @param Notification $notification
+	 * @param array $email_data Any additional e-mail data passed to Notification::issue function
+	 * @return array(subject, body)
+	 */
+	public function getEmail(Notification $notification, array $email_data)
+	{
+		global $txt;
+
+		return array($txt['notification_move_email_subject'], $this->getText($notification));
+	}
+
+	public function getIcon(Notification $notification)
+	{
+		global $memberContext;
+
+		$data = $notification->getData();
+		if (empty($data['member']['id']))
+			return '';
+		if (empty($memberContext[$data['member']['id']]['avatar']))
+		{
+			loadMemberAvatar($data['member']['id'], true);
+			if (empty($memberContext[$data['member']['id']]['avatar']))
+				return '';
+		}
+		return $memberContext[$data['member']['id']]['avatar']['image'];
+	}
+
+	public function getPreview(Notification $notification)
+	{
+		$data = $notification->getData();
+		return get_single_post($data['id_msg']);
+	}
+}
