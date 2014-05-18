d96e014a82ebd8a6da98b81d905b1eae2ef35e28
Nao <nao@wedge>
2014-05-18 21:26:09 +0200

    * Another cache query optimized. I love PHP 5.3. (Aeva-Gallery.php)

--- core/app/media/Aeva-Gallery.php
+++ core/app/media/Aeva-Gallery.php
@@ -1070,13 +1070,10 @@ function aeva_viewItem()
 		$item_data['is_resized'] = !empty($item_data['id_preview']);
 		preg_match('~\[url=([^]]+)~', $item_data['embed_url'], $match);
 		$key = (isset($match[1]) ? md5($match[1]) . '-' : '') . md5($item_data['embed_url']);
-		if (($item_data['embed_object'] = cache_get_data('aeva-embed-link-' . $key, 1200)) === null)
-		{
+		$item_data['embed_object'] = cache_get_data('aeva-embed-link-' . $key, 1200, function () use ($item_data) {
 			loadSource('media/Aeva-Embed');
-
-			$item_data['embed_object'] = aeva_embed_video($item_data['embed_url'], $item_data['id_media'], $item_data['id_preview']);
-			cache_put_data('aeva-embed-link-' . $key, $item_data['embed_object'], 1200);
-		}
+			return aeva_embed_video($item_data['embed_url'], $item_data['id_media'], $item_data['id_preview']);
+		});
 	}
 	else
 	{
